1141697a4ac12534e229b51bfc0352722805e25a
Dedesyarifudin07 <dedesyarifudinalhadid@gmail.com>
2022-12-01 06:43:47 +0700

    menambahakan komentar

--- midleware/verifyToken.js
+++ midleware/verifyToken.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req,res,next) => {
+    //request header authorazation
     const authHeader = req.headers['authorization'];
+    //bearer token
     const token = authHeader && authHeader.split(' ')[1];
     if (!token){
         return res.status(404).json({msg:'token not found'});
